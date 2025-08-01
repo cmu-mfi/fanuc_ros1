968e3cc629acb7ca7e16d3fa5535d0f46b8d597e
Ankit Aggarwal <ankitagga0612@gmail.com>
2025-08-01 21:25:49 +0000

    working tool0_pose

--- fc_tasks/src/fc_interface.cpp
+++ fc_tasks/src/fc_interface.cpp
@@ -354,30 +354,15 @@ void FC_Interface::checkTrajStatus(const actionlib_msgs::GoalStatusArray::ConstP
 }
 
 void FC_Interface::jointStatesCallback(const sensor_msgs::JointState::ConstPtr& msg){
-	// READ JOINT STATES
-	VectorJd q(msg->position.size());
-	for(int i=0; i<msg->position.size(); i++)
-	{
-		q(i) = msg->position[i];
-	}
 
-	// COMPUTE FORWARD KINEMATICS
-	Eigen::Matrix4d tool0_pose = _FK(q, robot_DH_, robot_base_frame_, true);
+        ros::AsyncSpinner async_spinner(1);                                                                                                                        async_spinner.start();
 
-	// PUBLISH TOOL0 POSE
-	geometry_msgs::PoseStamped tool0_pose_msg;
-	tool0_pose_msg.header.stamp = ros::Time::now();
-	tool0_pose_msg.header.frame_id = "/real/base_link";
-	tool0_pose_msg.pose.position.x = tool0_pose(0, 3);
-	tool0_pose_msg.pose.position.y = tool0_pose(1, 3);
-	tool0_pose_msg.pose.position.z = tool0_pose(2, 3);
-	Eigen::Quaterniond q_(tool0_pose.block<3, 3>(0, 0));
-	tool0_pose_msg.pose.orientation.x = q_.x();
-	tool0_pose_msg.pose.orientation.y = q_.y();
-	tool0_pose_msg.pose.orientation.z = q_.z();
-	tool0_pose_msg.pose.orientation.w = q_.w();
-	
-	tool0_pose_pub_.publish(tool0_pose_msg);
+	tool0_pose_pub_.publish(move_group_.getCurrentPose());
+
+	async_spinner.stop();
+
+	//        geometry_msgs::PoseStamped pose;
+	//        tool0_pose_pub_.publish(pose);
 }
 
 /** Private Methods */
@@ -483,7 +468,7 @@ bool FC_Interface::planAndExecuteCartesianPath_(const std::vector<geometry_msgs:
 
 		item.req = mp_req_base;
 		geometry_msgs::PoseStamped waypoint_pose;
-		waypoint_pose.header.frame_id = "base_link";
+		waypoint_pose.header.frame_id = "/real/base_link";
 		waypoint_pose.pose = waypoints[i];
 		moveit_msgs::Constraints goal = kinematic_constraints::constructGoalConstraints(move_group_.getEndEffectorLink(), waypoint_pose, pos_tolerance, orient_tolerance);
 		item.req.goal_constraints.push_back(goal);
--- le_classmate_ros/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='le_classmate_ros',
-    version='1.0.0',
-    packages=['le_classmate_ros'],
-    package_dir={'': 'src'},
-)
